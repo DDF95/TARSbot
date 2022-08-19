8dd8d229211e51b2acda86beb91d3ae067ac7b6e
DDF95 <difebbo.davide@gmail.com>
2022-08-19 18:27:16 +0200

    added !leave

--- plugins/joinchat.py
+++ plugins/joinchat.py
@@ -1,5 +1,13 @@
-from pyrogram import Client, filters, enums
+from configparser import ConfigParser
+
+from pyrogram import Client, enums, filters
 from pyrogram.errors.exceptions import bad_request_400, forbidden_403
+
+
+cfg = ConfigParser(interpolation=None)
+cfg.read("config.ini")
+
+ADMIN1_ID = int(cfg.get("admins", "admin1"))
 
 
 @Client.on_message(filters.command("join", "!"))
@@ -11,7 +19,7 @@ async def join_chat(client, message):
         try:
             chat_joined = await client.join_chat(chat_to_join)
             if chat_joined:
-                await message.reply(f"Joined \"{chat_joined.title}\".")
+                await message.reply(f"Joined \"{chat_joined.title}\".\nID: `{chat_joined.id}`")
         except bad_request_400.ChannelsTooMuch:
             await message.reply("Error: I joined in too many groups and channels!")
         except bad_request_400.UsernameNotOccupied:
@@ -20,3 +28,18 @@ async def join_chat(client, message):
             await message.reply("Error: group or channel not available!")
         except Exception as e:
             await message.reply(f"Error:\n<code>{e}</code>")
+
+
+@Client.on_message(filters.command("leave", "!"))
+async def leave_chat(client, message):
+    if len(message.command) == 1:
+        await client.leave_chat(message.chat.id)
+    else:
+        if message.from_user.id == ADMIN1_ID:
+            try:
+                await client.leave_chat(int(message.command[1]))
+                await message.reply("Left the group/channel.")
+            except Exception as e:
+                await message.reply(f"Error:\n<code>{e}</code>")
+        else:
+            await message.reply("You are not an admin!")
